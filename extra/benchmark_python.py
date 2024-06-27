163014ee08d958ddda33e293c058c7ab3a681da1
Mit Kotak <mitkotak0305@gmail.com>
2024-06-26 20:19:39 -0400

    added o3.experimental.FullTensorProductv2 + benchmaking and plotting nitpicks

--- extra/benchmark_python.py
+++ extra/benchmark_python.py
@@ -7,6 +7,7 @@ import torch
 import e3nn as e3nn_torch
 
 channels = 128
+warmup = 10
 trials = 100
 L = 8
 
@@ -20,8 +21,10 @@ def main():
         input2 = e3nn_jax.zeros(irreps2, ())
 
         tp = jax.jit(e3nn_jax.tensor_product)
+
         # perform once to jit
-        tp(input1, input2)
+        for _ in range(warmup):
+            output = tp(input1, input2)
 
         start = time.process_time()
 
@@ -43,11 +46,37 @@ def main():
             input2 = irreps2.randn(-1) * 0
 
             tp = e3nn_torch.o3.FullTensorProduct(irreps1, irreps2)
+            tp = e3nn_torch.util.jit.compile(tp)
+
             # let jit
-            tp(input1, input2)
+            # perform once to jit
+            for _ in range(warmup):
+                output = tp(input1, input2)
 
             start = time.process_time()
 
+            for _ in range(trials):
+                output = tp(input1, input2)
+
+            print(f"{lmax},", time.process_time() - start)
+
+    print("e3nn-torch2")
+    torch.set_num_threads(1) # note this actually makes it faster
+    with torch.no_grad():
+        for lmax in range(1, L):
+            irreps1 = e3nn_torch.o3.Irreps.spherical_harmonics(lmax)
+            irreps2 = (channels * irreps1).sort().irreps.simplify()
+
+            input1 = irreps1.randn(-1) * 0
+            input2 = irreps2.randn(-1) * 0
+            tp = e3nn_torch.o3.experimental.FullTensorProductv2(irreps1, irreps2)
+            tp = torch.compile(tp, fullgraph=True)
+
+            # perform once to jit
+            for _ in range(warmup):
+                output = tp(input1, input2)
+
+            start = time.process_time()
             for _ in range(trials):
                 output = tp(input1, input2)
 
