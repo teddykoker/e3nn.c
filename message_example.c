996765cac4a8e2fab85f0b2662cd9649582542d2
Mit Kotak <mitkotak0305@gmail.com>
2024-07-10 20:51:02 -0400

    message function

---
+++ message_example.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include "e3nn.h"
+
+int main(void){
+
+    float node_position_sh[9] = {0};
+    spherical_harmonics("1x0e + 1x1o + 1x2e", 1, 2, 3, node_position_sh);
+
+    float neighbor_feature[] = {7,8,9};
+    float product[27] = { 0 };
+    tensor_product("1x0e + 1x1o + 1x2e", node_position_sh, 
+                   "1x1e", neighbor_feature, 
+                   "1x0o + 1x1o + 2x1e + 1x2e + 1x2o + 1x3e", product);
+    printf("product ["); for (int i = 0; i < 27; i++){ printf("%.2f, ", product[i]); } printf("]\n");
+
+    float weights[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    ///               [ 1 x 1 weight] [1 x 1 weight] [2 x 2 weight] [1 x 1 weight] [1 x 1 weight] [ 1 x 1 weight]
+    float output[27] = { 0 };
+    linear("1x0o + 1x1o + 2x1e + 1x2e + 1x2o + 1x3e",
+           product,
+           weights,
+           "1x0o + 1x1o + 2x1e + 1x2e + 1x2o + 1x3e",
+           output);
+
+    printf("output ["); for (int i = 0; i < 27; i++) { printf("%.2f, ", output[i]); } printf("]\n");
+    
+    return 0;
+}
---
+++ message_example.py
@@ -0,0 +1,20 @@
+import jax.numpy as jnp
+import jax
+import e3nn_jax as e3nn
+
+# Computed message (for e.g. force) between two atoms
+
+node_position = jnp.asarray([1,2,3])
+node_position_sh = e3nn.spherical_harmonics("1x0e + 1x1o + 1x2e", node_position, normalize=True, normalization="component")
+
+neighbor_feature = e3nn.IrrepsArray("1x1e", jnp.asarray([7,8,9]))
+tp = e3nn.tensor_product(node_position_sh, neighbor_feature)
+linear = e3nn.flax.Linear("1x0o + 1x1o + 2x1e + 1x2e + 1x2o + 1x3e",
+                          "1x0o + 1x1o + 2x1e + 1x2e + 1x2o + 1x3e")
+weights = {'params': {'w[0,0] 1x0o,1x0o': jnp.asarray([[1]]),
+                      'w[1,1] 1x1o,1x1o': jnp.asarray([[2]]),
+                      'w[2,2] 2x1e,2x1e': jnp.asarray([[3 , 4], [ 5,  6]]),
+                      'w[3,3] 1x2e,1x2e': jnp.asarray([[7]]),
+                      'w[4,4] 1x2o,1x2o': jnp.asarray([[8]]),
+                      'w[5,5] 1x3e,1x3e': jnp.asarray([[9]])}}
+message = linear.apply(weights, tp)
